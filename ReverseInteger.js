3be2a86485677d3564fb4c7952243471f715832a
klinkmaster <42745139+klinkmaster@users.noreply.github.com>
2020-01-07 12:40:26 -0800

    Create ReverseInteger.js

---
+++ ReverseInteger.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    
+    /*
+      Get absolute value
+      convert number to string
+      split string
+      reverse order
+      join it back together
+    */
+    var str = Math.abs(x).toString().split().reverse().join();
+    
+    /*
+      if original value is negative, add "-"
+    */
+    var rev = Number(x < 0 ? "-"+str : str);
+    
+    /*
+      Set parameter within 32-bit range
+      anything outside that range returns "0"
+    */
+    if(Math.abs(x) > 2^31){
+        return "0";
+    }else{
+        return rev;
+    }
+};
